860be28116cb871f315b1bbf454209c977fd63b1
Priytesh Prasad <priyteshprasd@gmail.com>
2024-05-19 10:08:23 +0530

    sol1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+  function handleChange(e) {
+    setName(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input onChange={handleChange}></input>
+      {name.length > 0 ? <p>Hello {name}</p> : null}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
